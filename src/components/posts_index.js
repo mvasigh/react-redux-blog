1515ff297f5c02845a6a364ab811fac4c2f2d05a
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-08 17:36:05 -0500

    add Link component to each post list item

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -12,7 +12,7 @@ class PostsIndex extends Component {
   renderPosts() {
     return _.map(this.props.posts, post => (
       <li key={post.id} className="list-group-item">
-        {post.title}
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
       </li>
     ));
   }
