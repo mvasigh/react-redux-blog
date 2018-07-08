ffb58eba356180d62c755b3671b1bf6ec085e841
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-08 14:46:38 -0500

    render list of posts from redux state object

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -7,12 +9,34 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => (
+      <li key={post.id} className="list-group-item">
+        {post.title}
+      </li>
+    ));
+  }
+
   render() {
-    return <div>Posts Index</div>;
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostsIndex);
