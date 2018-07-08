e182c31394e2e626a396693041f3c0e35a7a71a3
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-08 14:47:48 -0500

    fix issue with postsReducer returning only the first post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,10 +3,10 @@ import { FETCH_POSTS } from '../actions';
 export default function postsReducer(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      return action.payload.data.reduce(
-        (obj, post) => (obj[post.id] = post),
-        {}
-      );
+      return action.payload.data.reduce((obj, post) => {
+        obj[post.id] = post;
+        return obj;
+      }, {});
     default:
       return state;
   }
