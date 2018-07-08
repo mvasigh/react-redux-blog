13d3450f6ccd16d15dee4dda90afc0e0cb5ed00b
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-08 17:39:18 -0500

    add FETCH_POST and DELETE_POST cases to posts reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,5 @@
-import { FETCH_POSTS } from '../actions';
+import _ from 'lodash';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function postsReducer(state = {}, action) {
   switch (action.type) {
@@ -7,6 +8,10 @@ export default function postsReducer(state = {}, action) {
         obj[post.id] = post;
         return obj;
       }, {});
+    case FETCH_POST:
+      return { ...state, [action.payload.data.id]: action.payload.data };
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
